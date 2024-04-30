c817388c38b3c14fa7ba0bdd69ed4e8a89208968
Abhijeet Sharnagat <abhijeetsharnagat200@gmail.com>
2024-04-30 14:06:50 +0530

    genix

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
-import CollegeImage from '../images/patil-Photoroom.png'; // Import your college image
+import CollegeImage from '../images/genix.png'; // Import your college image
 
 // Keyframes for animation
 const fadeIn = keyframes`
@@ -26,11 +26,11 @@ function Home() {
     <Box sx={{ bgcolor: 'background.default', color: 'text.primary', minHeight: '100vh' }}>
       <Container maxWidth="xl" sx={{ py: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Welcome to <AnimatedHeading>Ajeenkya Dy Patil Universitygram!</AnimatedHeading>
+          Welcome to <AnimatedHeading>Genix academy!</AnimatedHeading>
         </Typography>
-        <img src={CollegeImage} alt="Ajeenkya Dy Patil University" style={{ width: '100%', maxWidth: '800px', marginBottom: '40px', borderRadius: '8px', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }} /> {/* Add your college image */}
+        <img src={CollegeImage} alt="Ajeenkya Dy Patil University" style={{ height:'400px', width: '50%', maxWidth: '400px', marginBottom: '40px', borderRadius: '8px', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)' }} /> {/* Add your college image */}
         <Typography variant="body1" gutterBottom align="center">
-          Connect with your peers, share your thoughts, and explore the world of Ajeenkya Dy Patil University.
+          Connect with your peers, share your thoughts, and explore the world of Genix academy.
         </Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4, width: '100%' }}>
           <Button variant="contained" color="primary" component={Link} to="/login" sx={{ mr: 2 }}>
